8b18c6c4cd0e70bb5720f93d2b5eebef093440ba
valerikolev <127833084+valerikolev@users.noreply.github.com>
2023-06-02 09:24:57 +0300

    Add files via upload

---
+++ 10anemci/10anemci/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10anemci
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("kolko uchenici");
+           int n = int.Parse(Console.ReadLine());
+           
+            //int uchenici;
+            //Console.WriteLine("kolko uchenici");
+            //    uchenici = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {          
+ Console.WriteLine("na kolko godini e uchenika");
+            int godini = int.Parse(Console.ReadLine());
+             Console.WriteLine("koi mesec e roden uchenika");
+                  int mesec = int.Parse(Console.ReadLine());
+ 
+
+                if (godini == 14 )
+                {
+                    Console.WriteLine("Ti si v 8 klas");
+                    if (mesec <= 9 || mesec >= 1)
+                    {
+                        Console.WriteLine("Ti si v 8 klas");
+                    }
+
+                }
+                else
+                {
+                    if (godini >= 13)
+                    {
+                        Console.WriteLine("ne si za tuka");
+
+                    }
+                }
+               
+                if (godini == 15)
+                {
+                    Console.WriteLine("Ti si v 9 klas");
+                    if (mesec >= 9 || mesec <= 9)
+                    {
+                        Console.WriteLine("Ti si v 9 klas");
+                    }
+                }
+                else
+                {
+                    if (godini >= 13)
+                    {
+                        Console.WriteLine("ne si za tuka");
+                    }
+                }
+                if (godini == 16)
+                {
+                    Console.WriteLine("ti si v 10 klas");
+                    if (mesec <= 9 || mesec >= 1)
+                    {
+                        Console.WriteLine("ti si v 10 klas");
+                    }
+                }
+                else
+                {
+                    if (godini >= 13)
+                    {
+                        Console.WriteLine("ne si za tuka");
+                    }
+                }
+                if (godini == 17)
+                {
+                    Console.WriteLine("ti si v 11 klas");
+                    if (mesec >= 9 || mesec <= 9)
+                    {
+                        Console.WriteLine("ti si v 11 klas");
+                    }
+                }
+                else
+                {
+                    if (godini >= 13)
+                    {
+                        Console.WriteLine("ne si za tuka");
+                    }
+                }
+                if (godini == 18 )
+                {
+                    Console.WriteLine("ti si v 12 klas");
+                    if (mesec <= 9 || mesec >= 1)
+                    {
+                        Console.WriteLine("ti si v 12 klas");
+                    }
+                }
+                else
+                {
+                    if (godini >= 13 || mesec <= 9)
+                    {
+                        Console.WriteLine("ne si za tuka");
+                    }
+                }
+
+            }
+        }
+    }
+}
---
+++ zavedemie_valeri/zavedemie_valeri/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zavedemie_valeri
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Колко души сте?");
+            int хора = int.Parse(Console.ReadLine());
+           
+            for (int i = 0; i < 1; i++)
+            {
+                Console.WriteLine("колко салати ша поръчваш");
+                int salati  = int.Parse(Console.ReadLine());
+                Console.WriteLine("колко овчарски салати ша поръчваш ");
+                int owcharska = int.Parse(Console.ReadLine());
+                Console.WriteLine("колко млечни салати ша поръчваш");
+                int mlechna = int.Parse(Console.ReadLine());
+                Console.WriteLine("колко туршии ша поръчваш");
+                int turshii = int.Parse(Console.ReadLine());    
+            }
+            for (int i = 0; i < 1; i++)
+            {
+                Console.WriteLine("колко основни ша поръчваш");
+                int osnoowni = int.Parse(Console.ReadLine());
+                Console.WriteLine("колко кебапчета ша поръчваш ");
+                int kebapèta = int.Parse(Console.ReadLine());
+                Console.WriteLine("колко кюфтета ша поръчваш");
+                int kufteta = int.Parse(Console.ReadLine());
+                Console.WriteLine("колко колко агнета ша поръчваш");
+                int agneta = int.Parse(Console.ReadLine());
+
+            }
+            for (int i = 0; i < 1; i++)
+            {
+                Console.WriteLine("колко топли напитки  ша поръчваш");
+                int toplopiene = int.Parse(Console.ReadLine());
+                Console.WriteLine("колко чая ша поръчваш ");
+                int chai = int.Parse(Console.ReadLine());
+                Console.WriteLine("колко греени ракии ша поръчваш");
+                int rakiq = int.Parse(Console.ReadLine());
+                Console.WriteLine("колко колко кафета ша поръчваш");
+                int kafe = int.Parse(Console.ReadLine());
+                Console.WriteLine("колко студени напитки  ша поръчваш");
+                int studenopiene = int.Parse(Console.ReadLine());
+                Console.WriteLine("колко коли ша поръчваш ");
+                int kola = int.Parse(Console.ReadLine());
+                Console.WriteLine("колко води ша поръчваш");
+                int woda  = int.Parse(Console.ReadLine());
+                Console.WriteLine("колко колко бири ша поръчваш");
+                int biri= int.Parse(Console.ReadLine());
+
+            }
+            for (int i = 0; i < 1; i++)
+            {
+                Console.WriteLine("колко десерта ша поръчваш");
+                int osnoowni = int.Parse(Console.ReadLine());
+                Console.WriteLine("колко торти ша поръчваш ");
+                int torti = int.Parse(Console.ReadLine());
+                Console.WriteLine("колко салдоледа ша поръчваш");
+                int sladoled = int.Parse(Console.ReadLine());
+                Console.WriteLine("колко колко крем карамела ша поръчваш");
+                int karamel = int.Parse(Console.ReadLine());
+            }
+        }
+    }
+}
